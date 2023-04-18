be5b6d2511e5d44e8234a1692a9877cdd69eb320
Jacob Bolden <jlbolden.pro@gmail.com>
2023-04-18 15:39:01 -0500

    Update types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,15 +2,21 @@ import NextAuth, { type DefaultSession } from "next-auth";
 
 declare module "next-auth" {
  interface Session {
+  error?: "RefreshAccessTokenError";
+  access_token: string;
   user: { id?: string } & DefaultSession["user"];
-  accessToken?: string;
+ }
+
+ interface Account {
+  expires_in: number;
  }
 }
 
 declare module "next-auth/jwt" {
  interface JWT {
-  accessToken?: string;
-  refreshToken?: string;
-  accessTokenExpires?: number;
+  access_token?: string;
+  expires_at?: number;
+  refresh_token?: string;
+  error?: "RefreshAccessTokenError";
  }
 }
