3c5fca7a786b3569a59e250d699db08beab94b7c
Jacob Bolden <jlbolden.pro@gmail.com>
2023-04-19 15:52:11 -0500

    add refresh token

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import type { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions, TokenSet } from "next-auth";
 import NextAuth from "next-auth/next";
 import SpotifyProvider from "next-auth/providers/spotify";
 
@@ -28,41 +28,38 @@ export const authOptions: NextAuthOptions = {
      expires_at: Math.floor(Date.now() / 1000 + account.expires_in),
      refresh_token: account.refresh_token,
     };
+   } else if (token.expires_at && Date.now() < token.expires_at * 1000) {
+    return token;
+   } else {
+    try {
+     const url = new URL("https://accounts.spotify.com/api/token");
+     url.searchParams.set("client_id", process.env.SPOTIFY_CLIENT_ID);
+     url.searchParams.set("client_secret", process.env.SPOTIFY_CLIENT_SECRET);
+     url.searchParams.set("grant_type", "refresh_token");
+     url.searchParams.set("refresh_token", `${token.refresh_token}`);
+
+     const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+     });
+
+     const tokens: TokenSet & { expires_in: number } = await response.json();
+     if (!response.ok) throw tokens;
+
+     return {
+      ...token,
+      access_token: tokens.access_token,
+      expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
+      refresh_token: tokens.refresh_token ?? token.refresh_token,
+     };
+    } catch (error) {
+     console.error("Error refreshing access token", error);
+     return {
+      ...token,
+      error: "RefreshAccessTokenError" as const,
+     };
+    }
    }
-   return token;
-   // } else if (token.expires_at && Date.now() < token.expires_at * 1000) {
-   //  return token;
-   // } else {
-   //  try {
-   //   const url = new URL("https://accounts.spotify.com/api/token");
-   //   url.searchParams.set("client_id", process.env.SPOTIFY_CLIENT_ID);
-   //   url.searchParams.set("client_secret", process.env.SPOTIFY_CLIENT_SECRET);
-   //   url.searchParams.set("grant_type", "refresh_token");
-   //   url.searchParams.set("refresh_token", `${token.refresh_token}`);
-
-   //   const response = await fetch(url, {
-   //    method: "POST",
-   //    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-   //   });
-
-   //   console.log("yo");
-   //   const tokens: TokenSet & { expires_in: number } = await response.json();
-   //   if (!response.ok) throw tokens;
-
-   //   return {
-   //    ...token,
-   //    access_token: tokens.access_token,
-   //    expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
-   //    refresh_token: tokens.refresh_token ?? token.refresh_token,
-   //   };
-   //  } catch (error) {
-   //   console.error("Error refreshing access token", error);
-   //   return {
-   //    ...token,
-   //    error: "RefreshAccessTokenError" as const,
-   //   };
-   //  }
-   // }
   },
   async session({ session, token }) {
    session.error = token.error;
