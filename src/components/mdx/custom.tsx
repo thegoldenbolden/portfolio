bae64c8a3d605c614f27aa10bb21da7f2ce57512
Jacob Bolden <jlbolden.pro@gmail.com>
2023-05-05 10:24:57 -0500

    Support github markdown

--- src/components/mdx/custom.tsx
+++ src/components/mdx/custom.tsx
@@ -1,9 +1,25 @@
 import StyledLink from '@components/link/styled';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import remarkGfm from 'remark-gfm';
 
 const components = {
   a: (props: JSX.IntrinsicElements['a']) => (
     <StyledLink {...props}>{props.children}</StyledLink>
+  ),
+  ul: (props: JSX.IntrinsicElements['ul']) => (
+    <ul {...props} className="list-disc marker:text-tw-gray ml-4 space-y-2">
+      {props.children}
+    </ul>
+  ),
+  h1: (props: JSX.IntrinsicElements['h1']) => (
+    <h1 {...props} className="text-2xl font-bold">
+      {props.children}
+    </h1>
+  ),
+  h2: (props: JSX.IntrinsicElements['h2']) => (
+    <h2 {...props} className="text-xl font-bold">
+      {props.children}
+    </h2>
   ),
 };
 
@@ -12,6 +28,7 @@ export default function CustomMDX(props) {
     // @ts-expect-error Async Server Component Workaround
     <MDXRemote
       {...props}
+      options={{ mdxOptions: { remarkPlugins: [remarkGfm] } }}
       components={{ ...components, ...(props.components || {}) }}
     />
   );
