15fab71cc1c4f91c81e7c5b6baacee54a991a140
Jacob Bolden <60676197+thegoldenbolden@users.noreply.github.com>
2024-11-11 10:41:21 -0600

    ye (#20)
    * nomono
    
    * bump deps
    
    * removed imports

--- apps/v2/src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from 'next';
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    sitemap: 'https://jacobbolden.com/sitemap.xml',
-    host: 'https://jacobbolden.com',
-    rules: [{ userAgent: '*' }],
-  };
-}
